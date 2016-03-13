2af5857749fac11fac794c0fb26282baa001de4a
Paulius Ivaskevicius <jevix.thedev@gmail.com>
2016-03-13 16:51:26 +0200

    Finished

--- EditIni/main.cpp
+++ EditIni/main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
 using namespace std;
+
+int getVarValue(string variable, string filePath);
+string replaceStrChar(string str, const string& replace, char ch);
+
 int main()
 {
-	cout << "Zdarova" << endl;
+	int value = getVarValue("kappa", "settings.ini");
+	cout << "Value : " << value << endl;
 	return 0;
+	
+}
+string replaceStrChar(string str, const string& replace, char ch) {
+	size_t found = str.find_first_of(replace);
+	while (found != string::npos) 
+	{ 
+		str[found] = ch; 
+		found = str.find_first_of(replace, found + 1); 
+	}
+	return str; 
+}
+int getVarValue(string variable, string filePath) 
+{
+	int value;
+	string line, test;
+	ifstream duom(filePath);
+	if (duom.is_open())
+	{
+		while (getline(duom, line))
+		{
+			if (line.find(variable) != string::npos && line.find("[") == string::npos)
+			{		
+				line = replaceStrChar(line, "=", ' ');
+				istringstream str(line);
+				str >> variable>> value;
+			}
+		}
+		duom.close();
+	}
+	return value;
 }
