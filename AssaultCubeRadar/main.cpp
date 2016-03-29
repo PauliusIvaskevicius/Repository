3bfb0e6716151b19ba04011700a02697c30f003f
Paulius Ivaskevicius <jevix.thedev@gmail.com>
2016-03-29 19:02:53 +0300

    Very good update

--- AssaultCubeRadar/main.cpp
+++ AssaultCubeRadar/main.cpp
@@ -18,6 +18,7 @@ public:
 	float x, y;
 };
 
+
 void FindWindowTool(HWND& hGameWindow, HANDLE& hProcHandle, DWORD& dwProcID, string& GameStatus, LPCSTR LGameWindow);
 Vec2D RotatePoint(Vec2D pointToRotate, Vec2D centerPoint, float angle, bool angleInRadians);
 
@@ -28,6 +29,13 @@ HDC HDC_Desktop;
 
 HBRUSH PlayerBrush;
 HBRUSH EnemyBrush;
+
+LPDIRECT3DTEXTURE9 tex;
+LPD3DXSPRITE sprite;
+D3DXVECTOR3 position;
+D3DCOLOR color;
+bool spriteInit = true;
+Sprait gCom;
 
 Vec2D centerPoint;
 string GameName = "AssaultCube";
@@ -58,14 +66,14 @@ struct MyPlayer
 
 	void ReadInformation()
 	{
-CLocalPlayer = localPlayerAddr;
-ReadProcessMemory(hProcHandle, (PBYTE*)(localPlayerAddr + 0xF8), &Health, sizeof(int), 0);
-ReadProcessMemory(hProcHandle, (PBYTE*)(localPlayerAddr + 0x225), &Name, 16, 0);
-ReadProcessMemory(hProcHandle, (PBYTE*)(localPlayerAddr + 0x150), &SMGAmmo, sizeof(int), 0);
-ReadProcessMemory(hProcHandle, (PBYTE*)(localPlayerAddr + 0x32c), &Team, 1, 0);
-ReadProcessMemory(hProcHandle, (PBYTE*)(localPlayerAddr + 0x40), &angleY, sizeof(float), 0);
-ReadProcessMemory(hProcHandle, (LPCVOID)(localPlayerAddr + 0x4), &Position, sizeof(Position), NULL);
-ReadProcessMemory(hProcHandle, (LPCVOID)0x510D98, &NumOfPlayers, sizeof(NumOfPlayers), NULL);
+		CLocalPlayer = localPlayerAddr;
+		ReadProcessMemory(hProcHandle, (PBYTE*)(localPlayerAddr + 0xF8), &Health, sizeof(int), 0);
+		ReadProcessMemory(hProcHandle, (PBYTE*)(localPlayerAddr + 0x225), &Name, 16, 0);
+		ReadProcessMemory(hProcHandle, (PBYTE*)(localPlayerAddr + 0x150), &SMGAmmo, sizeof(int), 0);
+		ReadProcessMemory(hProcHandle, (PBYTE*)(localPlayerAddr + 0x32c), &Team, 1, 0);
+		ReadProcessMemory(hProcHandle, (PBYTE*)(localPlayerAddr + 0x40), &angleY, sizeof(float), 0);
+		ReadProcessMemory(hProcHandle, (LPCVOID)(localPlayerAddr + 0x4), &Position, sizeof(Position), NULL);
+		ReadProcessMemory(hProcHandle, (LPCVOID)0x510D98, &NumOfPlayers, sizeof(NumOfPlayers), NULL);
 
 	}
 }MyPlayer;
@@ -98,13 +106,8 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR pStr, int nCmd)
 	WNDCLASSEX wcx = { 0 };  //used for storing information about the wnd 'class'
 
 	wcx.cbSize = sizeof(WNDCLASSEX);
-	wcx.lpfnWndProc = WndProc;             //wnd Procedure pointer
-	wcx.hInstance = hInst;               //app instance
-										 //use 'LoadImage' to load wnd class icon and cursor as it supersedes the 
-										 //obsolete functions 'LoadIcon' and 'LoadCursor', although these functions will 
-										 //still work. Because the icon and cursor are loaded from system resources ie 
-										 //they are shared, it is not necessary to free the image resources with either 
-										 //'DestroyIcon' or 'DestroyCursor'.
+	wcx.lpfnWndProc = WndProc;            
+	wcx.hInstance = hInst;
 	wcx.hIcon = reinterpret_cast<HICON>(LoadImage(0, IDI_APPLICATION,
 		IMAGE_ICON, 0, 0, LR_SHARED));
 	wcx.hCursor = reinterpret_cast<HCURSOR>(LoadImage(0, IDC_ARROW,
@@ -122,10 +125,10 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR pStr, int nCmd)
 	int desktopwidth = GetSystemMetrics(SM_CXSCREEN);
 	int desktopheight = GetSystemMetrics(SM_CYSCREEN);
 
-	HWND hwnd = CreateWindowEx(WS_EX_NOACTIVATE,                     //extended styles
+	HWND hwnd = CreateWindowEx( 0,                     //extended styles
 		classname.c_str(),     //name: wnd 'class'
 		_T("AssaultCube Radar By NobodyLTU(TheKirvis)"),   //wnd title
-		WS_OVERLAPPEDWINDOW,   //wnd style
+		WS_OVERLAPPED | WS_MINIMIZEBOX | WS_SYSMENU,   //wnd style
 		desktopwidth / 4,        //position:left
 		desktopheight / 4,       //position: top
 		500,        //width
@@ -154,9 +157,16 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR pStr, int nCmd)
 	centerPoint.x = m_Rect.right - m_Rect.left;
 	centerPoint.y = m_Rect.bottom - m_Rect.top;
 
+
+	
+
+
 	if (!gDX.D3DInit(hwnd))
 		return 0;
-	
+
+	gCom.Init(0, -20, "radar.png");
+
+
 	MSG msg;
 	while (GetMessage(&msg, 0, 0, 0)>0)
 	{
@@ -177,59 +187,69 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_PAINT:
 		if (gDX.dx_Device != NULL)
 		{
-			gDX.dx_Device->Clear(NULL, NULL, D3DCLEAR_TARGET, D3DCOLOR_RGBA(0, 0, 0, 0), 1.0f, NULL);
-			gDX.dx_Device->BeginScene();
-
-			gDX.FillRGBA((centerPoint.x / 2), (centerPoint.y / 2) - 4,	 2 * zoom, 4 * zoom, 0, 255, 255, 255);
-			gDX.FillRGBA((centerPoint.x / 2), (centerPoint.y / 2),		 3 * zoom, 3 * zoom, 255, 255, 255, 255);
-			if (GetAsyncKeyState(VK_OEM_PLUS))
-				zoom += 0.1;
-			if (GetAsyncKeyState(VK_OEM_MINUS))
+			
+			if (spriteInit)
 			{
-				if (zoom - 0.1 <= 0)
-					zoom = 0.1;
-				zoom -= 0.1;
+				
+				gDX.dx_Device->Clear(NULL, NULL, D3DCLEAR_TARGET, D3DCOLOR_RGBA(255, 255, 255, 255), 1.0f, NULL);
+				gDX.dx_Device->BeginScene();
+	
 
-			}
-
-			for (int i = 0; i < NumOfPlayers; i++)
-			{
-				MyPlayer.ReadInformation();
-				PlayerList[i].ReadInformation(i);
-
-				if (PlayerList[i].Health <= 0)
-					continue;
-
-				Vec2D PlayerListPos;
-				PlayerListPos.x = PlayerList[i].Position.x * zoom;
-				PlayerListPos.y = PlayerList[i].Position.y * zoom;
-
-				Vec2D MyPlayer2D;
-				MyPlayer2D.x = MyPlayer.Position.x * zoom;
-				MyPlayer2D.y = MyPlayer.Position.y * zoom;
-
-				Vec2D PositionToDraw = RotatePoint(PlayerListPos, MyPlayer2D, MyPlayer.angleY, false);
-
-				if (MyPlayer.Team == PlayerList[i].Team)
+				gCom.Draw();
+				gDX.FillRGBA((centerPoint.x / 2)  - ((2 * zoom) / 2), (centerPoint.y / 2) - 8, 4 * zoom, 8 * zoom, 0, 255, 255, 255);
+				//gDX.FillRGBA((centerPoint.x / 2), (centerPoint.y / 2), 3 * zoom, 3 * zoom, 255, 255, 255, 255);
+				if (GetAsyncKeyState(VK_OEM_PLUS))
+					zoom += 0.1;
+				if (GetAsyncKeyState(VK_OEM_MINUS))
 				{
-					gDX.FillRGBA(
-						PositionToDraw.x + (250.0 - MyPlayer2D.x),
-						PositionToDraw.y + (250.0 - MyPlayer2D.y),
-						5 * zoom, 5 * zoom, 23, 189, 40, 0);
+					if (zoom - 0.1 <= 0)
+						zoom = 0.1;
+					zoom -= 0.1;
+
 				}
-				else
+
+				for (int i = 0; i < NumOfPlayers; i++)
 				{
-					gDX.FillRGBA(
-						PositionToDraw.x + (250.0 - MyPlayer2D.x),
-						PositionToDraw.y + (250.0 - MyPlayer2D.y),
-						5 * zoom, 5 * zoom, 255, 0, 0, 0);
+					MyPlayer.ReadInformation();
+					PlayerList[i].ReadInformation(i);
+
+					if (PlayerList[i].Health <= 0)
+						continue;
+
+					Vec2D PlayerListPos;
+					PlayerListPos.x = PlayerList[i].Position.x * zoom;
+					PlayerListPos.y = PlayerList[i].Position.y * zoom;
+
+					Vec2D MyPlayer2D;
+					MyPlayer2D.x = MyPlayer.Position.x * zoom;
+					MyPlayer2D.y = MyPlayer.Position.y * zoom;
+
+					Vec2D PositionToDraw = RotatePoint(PlayerListPos, MyPlayer2D, MyPlayer.angleY, false);
+
+					if (MyPlayer.Team == PlayerList[i].Team)
+					{
+						gDX.FillRGBA(
+							PositionToDraw.x + (250.0 - MyPlayer2D.x),
+							PositionToDraw.y + (250.0 - MyPlayer2D.y),
+							5 * zoom, 5 * zoom, 255,127,80, 0);
+					}
+					else
+					{
+						gDX.FillRGBA(
+							PositionToDraw.x + (250.0 - MyPlayer2D.x),
+							PositionToDraw.y + (250.0 - MyPlayer2D.y),
+							5 * zoom, 5 * zoom, 255, 0, 0, 0);
+					}
+
+
+
 				}
 				
-
+				gDX.dx_Device->EndScene();
+				gDX.dx_Device->PresentEx(NULL, NULL, NULL, NULL, NULL);
 
 			}
-			gDX.dx_Device->EndScene();
-			gDX.dx_Device->PresentEx(NULL, NULL, NULL, NULL, NULL);
+			
 		}
 		
 
